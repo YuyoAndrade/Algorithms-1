09f0e6cf8b42f541ea2cb980c64693b936bb07ea
YuyoAndrade <euyuyo01@gmail.com>
2024-02-21 18:32:07 +0100

    Add comments

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,13 +1,13 @@
 # Insertion Sort
 
 def insertion_sort(A):
-    for j in range(1, len(A)):
-        key = A[j]
-        i = j - 1
-        while i >= 0 and A[i] > key:
-            A[i + 1] = A[i]
-            i -= 1
-        A[i+1] = key
+    for j in range(1, len(A)): # Start from index 1 since the first time is already ordered.
+        key = A[j] # Store current value
+        i = j - 1 # Pointer to left value
+        while i >= 0 and A[i] > key: # Check if the number on the left is smaller than key or end of line.
+            A[i + 1] = A[i] # Move value to the right
+            i -= 1 #
+        A[i+1] = key # Insert key once it is the smallest value or the left is smallest
     return A
 
 
